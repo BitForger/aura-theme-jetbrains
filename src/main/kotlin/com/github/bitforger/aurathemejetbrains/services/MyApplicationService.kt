8eb928e0e80fed3add949c66175710d89e5b1c9d
GitHub Action <action@github.com>
2021-10-15 03:20:23 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/bitforger/aurathemejetbrains/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.bitforger.aurathemejetbrains.services
+
+import com.github.bitforger.aurathemejetbrains.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/bitforger/aurathemejetbrains/services/MyProjectService.kt
+++ src/main/kotlin/com/github/bitforger/aurathemejetbrains/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.bitforger.aurathemejetbrains.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.bitforger.aurathemejetbrains.MyBundle
 
 class MyProjectService(project: Project) {
 
